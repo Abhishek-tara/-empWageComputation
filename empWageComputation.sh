6550dcadf1f574dd41c8e1e14f7b3ac03ea43cb0
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-01-17 15:45:37 +0530

    Add: employee attendance check

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,1 +1,13 @@
-echo "Welcome to the empWageComputation maser branch" 
+echo "Welcome to the empWageComputation on maser branch"
+
+isPresent=1;
+randomCheck=$((RANDOM%2));
+
+if [ $isPresent -eq $randomCheck ];
+then
+	echo "Employee is Present";
+else
+	echo "Employee is absent";
+fi
+
+ 
