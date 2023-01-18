a9bd800e51c0ccc0484e247fc1fcb94b8fbb02de
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-01-18 19:27:12 +0530

    REFACTOR: Adding function for employee wage calculation

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -8,22 +8,26 @@ num_Working_Days=20;
 totalEmpHrs=0;
 totalWorkingDays=0;
 
+function getWorkingHours(){
+     case $1 in
+         $is_Full_Time)
+          workHours=8
+          ;;
+         $is_Part_Time)
+          workHours=4
+          ;;
+         *)
+ 	  workHours=0
+      	  ;;
+     esac
+echo $workHours
+}
+
 while [[ $totalEmpHrs -lt $max_Hrs_In_Month && $totalWorkingDays -lt $num_Working_Days  ]]
 do
 	((totalWorkingDays++))
-	empCheck=$((RANDOM%3));
-		case $empCheck in
-			$is_Full_Time)
-			empHrs=8
-			;;
-			$is_Part_Time)
-			empHrs=4
-			;;
-			*)
-			empHrs=0
-			;;
-		esac
-		totalEmpHrs=$(($totalEmpHrs+$empHrs));
+	workHours=$( getWorkingHours $((RANDOM%3)) )
+        totalEmpHrs=$(($totalEmpHrs+$workHours))
 done
 
 totalSalary=$(($totalEmpHrs*$emp_Rate_Per_Hr));
