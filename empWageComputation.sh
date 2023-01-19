6889dfed23564573f2461f6529acbf948f657569
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-01-19 13:35:17 +0530

    Add: Storing Daily wage, Day and Total wage with Dictionary

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,42 +1,48 @@
+echo "Welcome to Employee Wage Computation Program"
 
-is_Part_Time=1;
-is_Full_Time=2;
-max_Hrs_In_Month=100;
-emp_Rate_Per_Hr=20;
-num_Working_Days=20;
+isPartTime=1;
+isFullTime=2;
+empHrs=0;
+totalSalary=0;
+totalWorkingHour=0;
+empRatePerHr=20;
+numWorkingDays=20;
+day=1;
 
-totalEmpHrs=0;
-totalWorkingDays=0;
-
-function getWorkingHours(){
-     case $1 in
-         $is_Full_Time)
-          workHours=8
-          ;;
-         $is_Part_Time)
-          workHours=4
-          ;;
-         *)
- 	  workHours=0
-      	  ;;
-     esac
-echo $workHours
+declare -A day
+function calculateWorkingHour(){
+	case $randomCheck in 
+		     $isFullTime)
+                        empHrs=8;;
+                     $isPartTime)
+                        empHrs=4;;
+                     *)
+                        empHrs=0;;
+esac
+	echo $empHrs;
 }
 
-function getEmpWage(){
-	local empHr=$1
-	echo $(($empHr*$emp_Rate_Per_Hr))
-}
+while [[ $day -le 20 && $totalWorkingHour -lt 100 ]]
 
-while [[ $totalEmpHrs -lt $max_Hrs_In_Month && $totalWorkingDays -lt $num_Working_Days  ]]
 do
-	((totalWorkingDays++))
-	workHours=$( getWorkingHours $((RANDOM%3)) )
-        totalEmpHrs=$(($totalEmpHrs+$workHours))
-	dailyWage[$totalWorkingDays]="$( getEmpWage $workHours)"
+randomCheck=$((RANDOM%3));
+
+	wHour=$(calculateWorkingHour $randomCheck);
+	totalWorkingHour=$(($totalWorkingHour + $wHour));
+
+	if [ $totalWorkingHour -gt 100 ]
+	then
+		totalWorkingHour=$(($totalWorkingHour-$wHour))
+		break;
+	fi
+
+	salary=$(($wHour*$empRatePerHr))
+	totalSalary=$(($totalSalary+$salary))
+	((day++));
+echo "Daily Wage: " ${salary[*]}
+echo "Total Salary: " ${totalSalary[*]}
+echo "Day; " ${day[*]}
 done
 
-totalSalary=$(($totalEmpHrs*$emp_Rate_Per_Hr));
-echo "Daily Wage = " ${dailyWage[@]} 
-echo "The Total Montly Employee Wage as per working hours or days is $totalSalary"
+echo "Employee has earned $totalSalary $ in a month (Total working Hour : $totalWorkingHour";
 
